424f01b97bf13bb0a02c88fc375005f78a849a0c
abethel912 <abethel180@gmail.com>
2023-02-28 22:23:22 -0500

    Rendered card component to index page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,28 @@
-const Card = (props) => {
-  return <h1>Card</h1>
+import { Link } from 'react-router-dom'
+
+//destructure the post from props
+const Card = ({ card }) => {
+  //////////////////
+  // Style Objects
+  //////////////////
+  const div = {
+    textAlign: 'center',
+    border: '3px solid',
+    margin: '10px auto',
+    width: '80%'
+  }
+  return (
+    <div style={div}>
+      <Link to={`/${card.id}`}>
+        <h1>Recipe:{card.name}</h1>
+      </Link>
+      <img src={card.img} className="recipe-image" />
+      <p>Cuisine: {card.cuisine}</p>
+      <p>Ingredients: {card.ingredients}</p>
+      <p>Directions: {card.directions}</p>
+      <p>Prep Time(Mins) {card.time}</p>
+    </div>
+  )
 }
 
 export default Card
